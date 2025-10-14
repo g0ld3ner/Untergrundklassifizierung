48a0f0e2de69a163e75a1c590c7663a66194df57
g0ld3ner <8351847+g0ld3ner@users.noreply.github.com>
2025-10-14 21:05:58 +0200

    Parameter für das Resampling der config hinzugefügt. Config Validierung angepasst auch bei optionalen Feldern auf den richtigen Typ zu prüfen.

--- src/untergrund/config.py
+++ src/untergrund/config.py
@@ -6,28 +6,46 @@ _REQUIRED = {
     "sensor_list": list,
 }
 
+_OPTIONAL = {
+    "resample_imu": dict,
+    "resample_location": dict,
+}
+
 def validate_config(cfg: dict[str, Any]) -> dict[str, Any]:
     '''
-    Validiert die Konfigurationsdatei. Prüft auf erforderliche Felder und deren Typen.
+    Validiert die Konfigurationsdatei.
+    Prüft ob alle erforderlichen Felder vorhanden sind.
+    Erforderliche und optionale Felder werden auf den richtigen Typ geprüft.
     '''
     # ist cfg ein dict?
     if not isinstance(cfg, dict):
         raise ValueError("Config must be a dict")
 
-    checked = set() #überprüfte Felder
+    req_checked = set() #überprüfte Felder
     # stimmt der Typ der Pflichtfelder?
     for key, expected_type in _REQUIRED.items():
         if key not in cfg:
-            raise ValueError(f"Missing config key: {key}")
+            raise ValueError(f"Missing required config key: {key}")
         if not isinstance(cfg[key], expected_type):
             raise ValueError(
-                f"Wrong type for '{key}': expected {expected_type.__name__}, "
+                f"Wrong type for required '{key}': expected {expected_type.__name__}, "
                 f"got {type(cfg[key]).__name__}"
             )
-        checked.add(key)
+        req_checked.add(key)
+
+    opt_checked = set()
+    # stimmt der Typ der optionalen Felder?
+    for key, expected_type in _OPTIONAL.items():
+        if key in cfg:
+            if not isinstance(cfg[key], expected_type):
+                raise ValueError(
+                    f"Wrong type for optional '{key}': expected {expected_type.__name__}, "
+                    f"got {type(cfg[key]).__name__}"
+                )
+            opt_checked.add(key)
 
     # nicht überprüfte Felder melden
-    unchecked = sorted(set(cfg.keys()) - checked)
+    unchecked = sorted(set(cfg.keys()) - req_checked - opt_checked)
     if unchecked:
         print(f"[validate_config] Unchecked config keys: {unchecked}")
 
